4b303bfd7db6eff26dbe39110ba010933372e7ec
yannis94 <yannisbgci@protonmail.com>
2024-04-14 00:15:31 +0200

    feat(dap): adding debugger (for Go)
    adding debugger (for Go)
    
    Signed-off-by: yannis94 <yannisbgci@protonmail.com>

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,31 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'leoluz/nvim-dap-go',
+    'nvim-neotest/nvim-nio',
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    require('dapui').setup()
+    require('dap-go').setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, { desc = '[D]ebuging [T]oggle breakpoint' })
+    vim.keymap.set('n', '<Leader>dc', dap.continue, { desc = '[D]ebuging [C]ontinue' })
+  end,
+}

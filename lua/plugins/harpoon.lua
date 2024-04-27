2ad94e38639eec56c63326151c9211a6a7e0d5fd
yannis94 <yannisbgci@protonmail.com>
2024-04-27 10:26:25 +0200

    feat(harpoon.lua): adding harpoon plugin
    adding harpoon plugin
    
    Signed-off-by: yannis94 <yannisbgci@protonmail.com>

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,40 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set('n', '<leader>ha', function()
+      harpoon:list():add()
+    end, { desc = '[H]arpoon [A]dd file' })
+    vim.keymap.set('n', '<C-e>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Toggle Harpoon' })
+
+    vim.keymap.set('n', '<C-h>', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<C-t>', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '<C-s>', function()
+      harpoon:list():select(4)
+    end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<leader>hp', function()
+      harpoon:list():prev()
+    end, { desc = '[H]arpoon [P]revious' })
+    vim.keymap.set('n', '<leader>hn', function()
+      harpoon:list():next()
+    end, { desc = '[H]arpoon [N]ext' })
+  end,
+}

9c158704e5034d1e9c2b1fac58f662e3b8242b30
yannis94 <yannisbgci@protonmail.com>
2024-10-13 12:03:22 +0200

    feat: add colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -40,4 +40,17 @@ return {
       -- vim.cmd.colorscheme 'rose-pine'
     end,
   },
+  {
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require('everforest').setup {
+        variant = 'hard',
+      }
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
 }
